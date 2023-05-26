267d5fb5eb0d0ed5476ac91c3fd1454f862ea555
Naim Sulejmani <naim.sulejmani@radix.com>
2023-05-26 17:10:16 +0200

    asdsadasd

--- src/main/java/com/example/oopjavafxg2/MainApplication.java
+++ src/main/java/com/example/oopjavafxg2/MainApplication.java
@@ -3,19 +3,22 @@ package com.example.oopjavafxg2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader =
-                new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+                new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Login to Cacttus Education!");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/oopjavafxg2/MainController.java
+++ src/main/java/com/example/oopjavafxg2/MainController.java
@@ -1,2 +1,30 @@
-package com.example.oopjavafxg2;public class MainController {
+package com.example.oopjavafxg2;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class MainController {
+
+    @FXML
+    private TextField txtUserName;
+    @FXML
+    private PasswordField txtPassword;
+    @FXML
+    private CheckBox chbSave;
+
+    public void login() {
+        System.out.printf("%s => %s %b %n",
+                txtUserName.getText(),
+                txtPassword.getText(),
+                chbSave.isSelected()
+        );
+    }
+
+    public void cancel() {
+        System.exit(0);
+    }
+
 }
